8cb644cf801eeb498da90d4b15228259d70d4adf
Morten Bendeke <mbendeke@hotmail.com>
2022-04-05 10:18:38 +0200

    lavet bottomMapper og bottom entity

--- src/main/java/dat/startcode/control/Login.java
+++ src/main/java/dat/startcode/control/Login.java
@@ -1,8 +1,10 @@
 package dat.startcode.control;
 
 import dat.startcode.model.config.ApplicationStart;
+import dat.startcode.model.entities.Bottom;
 import dat.startcode.model.entities.Customer;
 import dat.startcode.model.exceptions.DatabaseException;
+import dat.startcode.model.persistence.BottomMapper;
 import dat.startcode.model.persistence.CustomerMapper;
 import dat.startcode.model.persistence.ConnectionPool;
 
@@ -13,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
